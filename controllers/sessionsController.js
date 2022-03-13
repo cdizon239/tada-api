9707625abdaf279e2ed4111fe8c3f991b1afe5f6
cdizon239 <coleencharmille@gmail.com>
2022-03-12 22:28:24 -0500

    add logout route to destroy sessions

--- controllers/sessionsController.js
+++ controllers/sessionsController.js
@@ -59,5 +59,15 @@ router.post('/signIn', async (req, res) => {
     }
 })
 
+router.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.status(400).json({message: err.message})
+        } else {
+            return res.status(200).json({message: "Signed out successfuly"})
+        }
+    })
+})
+
 
 module.exports = router
